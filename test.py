8b9768d9b9215ab44193f817e6a5e7c5f9a46725
Astou03 <ndeyeastoun1309@gmail.com>
2024-10-28 08:51:38 +0100

    Ajout d'un paramètre n pour la fonction affiche.

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import unittest
 from fizz import affiche
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_affiche_base(self):
-        result = affiche()
-        expected = '12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz...9798FizzBuzz'
+    def test_affiche_param(self):
+        result = affiche(15)
+        expected = '12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee'
         self.assertEqual(result, expected)
 
 if __name__ == "__main__":
