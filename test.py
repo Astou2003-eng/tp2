878a0884cec55304c45d6551f0e64af7ec16a029
Astou03 <ndeyeastoun1309@gmail.com>
2024-10-28 08:58:09 +0100

    Ajout de la fonctionnalité d'intervalle n1 à n2 pour affiche().

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import unittest
 from fizz import affiche
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_affiche_param(self):
-        result = affiche(15)
-        expected = '12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee'
+    def test_affiche_intervalle(self):
+        result = affiche(5, 10)
+        expected = 'BuzzFizz78FizzBuzz'
         self.assertEqual(result, expected)
 
 if __name__ == "__main__":
